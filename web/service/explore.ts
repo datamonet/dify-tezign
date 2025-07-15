f7d64682be388027b7e184f5337340a976486768
faye <faye_1225@163.com>
2025-07-15 11:45:58 +0800

    add:explore function

--- web/service/explore.ts
+++ web/service/explore.ts
@@ -1,5 +1,6 @@
 import { del, get, patch, post } from './base'
 import type { App, AppCategory } from '@/models/explore'
+import type { CommonResponse } from '@/models/common'
 
 export const fetchAppList = () => {
   return get<{
@@ -38,4 +39,14 @@ export const updatePinStatus = (id: string, isPinned: boolean) => {
 
 export const getToolProviders = () => {
   return get('/workspaces/current/tool-providers')
+}
+
+// takin code:增加推荐
+export const createRecommendedApp = (id: string, description?: string, category?: string) => {
+  return post<App>('/explore/apps', { body: { app_id: id, description, category } })
+}
+
+// takin code:删除推荐
+export const deleteRecommendedApp = (id: string) => {
+  return del<CommonResponse>(`/explore/apps/${id}`)
 }
